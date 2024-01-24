bceafced89d981472805448a804f3effc35118d9
SIDDARTH-P-P <sidhusidharth2711@gmail.com>
2024-01-24 18:54:21 +0530

    Home page created

---
+++ src/components/body.jsx
@@ -0,0 +1,54 @@
+export default function Body() {
+    return (
+        <>
+            <div className="px-[30px] mt-[40px]">
+                <div className="flex justify-between">
+                    <span >Spotify playlist</span><span>Show all</span>
+                </div>
+            </div>
+            <div className="grid grid-cols-5 gap-5 px-[20px]">
+
+            <div className="bg-blue-300 rounded-[10px]">
+                    <div className="p-[20px]">
+                        <img className="w-full h-full rounded-[15px]" src="./spo.jpg" alt="" />
+                        <h3>Dinner with friend</h3>
+                        <p className=" max-h-[20px] overflow-hidden">text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy</p>
+                    </div>
+                </div>
+
+                <div className="bg-blue-300 rounded-[10px]">
+                    <div className="p-[20px]">
+                        <img className="w-full h-full rounded-[15px]" src="./spo.jpg" alt="" />
+                        <h3>Dinner with friend</h3>
+                        <p className=" max-h-[20px] overflow-hidden">text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy</p>
+                    </div>
+                </div>
+
+                <div className="bg-blue-300 rounded-[10px]">
+                    <div className="p-[20px]">
+                        <img className="w-full h-full rounded-[15px]" src="./spo.jpg" alt="" />
+                        <h3>Dinner with friend</h3>
+                        <p className=" max-h-[20px] overflow-hidden">text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy</p>
+                    </div>
+                </div>
+
+                <div className="bg-blue-300 rounded-[10px]">
+                    <div className="p-[20px]">
+                        <img className="w-full h-full rounded-[15px]" src="./spo.jpg" alt="" />
+                        <h3>Dinner with friend</h3>
+                        <p className=" max-h-[20px] overflow-hidden">text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy</p>
+                    </div>
+                </div>
+
+                <div className="bg-blue-300 rounded-[10px]">
+                    <div className="p-[20px]">
+                        <img className=" w-full h-full rounded-[15px]" src="./spo.jpg" alt="" />
+                        <h3>Dinner with friend</h3>
+                        <p className=" max-h-[20px] overflow-hidden">text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy</p>
+                    </div>
+                </div>
+
+            </div>
+        </>
+    );
+}
--- src/components/home_nav.jsx
+++ src/components/home_nav.jsx
@@ -1,12 +1,18 @@
+import Body from "./body";
+
 export default function Homenav() {
     return (
         <>
-            <nav className="bg-red-500">
-                <div className="flex justify-end pe-[20px] py-[10px]">
+            <nav className="bg-red-500 grid grid-cols-2 gap-4">
+                <div className="py-[7px] ps-[20px]">
+                <span><img className="bg-orange-500 rounded-[50px]" src="./left_arrow.svg" alt="" /></span> <span><img className="bg-orange-500 rounded-[50px]" src="./right_arrow.svg" alt="" /></span>
+                </div>
+                <div className="flex justify-end pe-[20px] py-[7px]">
                     <button className="me-[20px]">Sign up</button>
-                    <button>Login</button>
+                    <button className=" rounded-[15px] p-[10px]">Login</button>
                 </div>
             </nav>
+            <Body/>
         </>
     );
 }
